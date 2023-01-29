a7ea709b4783cd2617ba383ea8b48c4d61367340
F_Unction <51352133+F-Unction@users.noreply.github.com>
2023-01-29 12:35:50 +0800

    Update connect.py

--- connect.py
+++ connect.py
@@ -4,3 +4,5 @@ import subprocess
 for line in subprocess.getoutput('gh cs list').splitlines():
     line=line.split("\t")[0]
     print(line + subprocess.getoutput('gh cs ssh -c '+ line +' \"echo ok && exit\"'))
+    subprocess.run('gh codespace stop -c ' + line)
+    
